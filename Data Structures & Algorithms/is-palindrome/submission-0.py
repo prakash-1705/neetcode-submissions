0d436a7070b4d38cd0f3ca5315046a06ed42cba6
prakash-1705 <brlprakash985@gmail.com>
2026-09-01 07:45:31 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        s = "".join(char.casefold() for char in s if char.isalnum())
+        right = len(s)-1
+        while left<right:
+            if(s[left]!=s[right]):
+                return False
+            left += 1
+            right -= 1
+        return True
